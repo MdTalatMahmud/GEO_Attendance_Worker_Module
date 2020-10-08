23c965ef2491c8cf5e4272bb455c822c796d613b
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-10-08 22:06:34 +0600

    -menu added

--- app/src/main/java/com/example/geoattendanceworkermodule/DashboardActivity.java
+++ app/src/main/java/com/example/geoattendanceworkermodule/DashboardActivity.java
@@ -3,6 +3,8 @@ package com.example.geoattendanceworkermodule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
