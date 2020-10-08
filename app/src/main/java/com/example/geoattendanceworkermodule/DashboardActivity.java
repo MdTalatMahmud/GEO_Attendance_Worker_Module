29b0de3c42b867efcca958355d2bf68dfab27ec8
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-10-08 22:31:25 +0600

    -menu added

--- app/src/main/java/com/example/geoattendanceworkermodule/DashboardActivity.java
+++ app/src/main/java/com/example/geoattendanceworkermodule/DashboardActivity.java
@@ -1,10 +1,15 @@
 package com.example.geoattendanceworkermodule;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -19,5 +24,18 @@ public class DashboardActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_layout, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.logoutItemID){
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            Intent intent=new Intent(DashboardActivity.this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
